b3e1cce37cc7b21949313ef629a12f7cb307379e
Maycol Bedoya <mayki1992@gmail.com>
2020-07-29 22:47:06 -0500

    new component calendar

---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+import * as Moment from 'moment';
+import { extendMoment } from 'moment-range';
+const moment = extendMoment(Moment);
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(moment().format());
+  }
+
+}
