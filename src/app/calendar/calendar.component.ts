c45d35897d91a1deb766b2d368e6c206bacb853c
Maycol Bedoya Cabrera <mayki1992@gmail.com>
2020-08-11 22:13:21 -0500

    Created the interface and the functions to fill the calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -12,29 +12,61 @@ const moment = extendMoment(Moment);
 export class CalendarComponent implements OnInit {
 
   objElement = {};
+  infoCalendar=[];
   constructor() { }
 
   ngOnInit() {
-    this.objElement={
-      date:moment().format('MMMM YYYY'),
-      formatDate:moment()
+    this.objElement = {
+      date: moment().format('MMMM YYYY'),
+      formatDate: moment()
     };
     this.initCalendar(this.objElement['formatDate']);
   }
-  initCalendar(actualDate:Object){
-    console.log(actualDate);
+  initCalendar(actualDate: Object) {
+    const initWeek = moment(actualDate).startOf('month').startOf('isoWeek');
+    const endWeek = moment(actualDate).endOf('month').endOf('isoWeek');
+    const initMonth = moment(actualDate).startOf('month');
+    const endMonth = moment(actualDate).endOf('month');
+
+    const dateRange = moment.range(initWeek, endWeek);
+    const daysArray = Array.from(dateRange.by('days'));
+    const weeksArray = Array.from(dateRange.by('weeks'));
+
+    let objDates;
+    const array = [];
+
+    _.forEach(weeksArray, function (value) {
+      objDates = {
+        week: value.isoWeek(),
+        days: []
+      }
+      _.forEach(daysArray, function (day) {
+        if (value.isoWeek() === day.isoWeek()) {
+          objDates.days.push({
+            formatDate: day,
+            date: day.format('DD-MM-YYYY'),
+            week: day.isoWeek(),
+            day: day.format('DD'),
+            actualDate: moment(day.format('DD-MM-YYYY')).isSame(moment().format('DD-MM-YYYY')),
+            isActualMonth: day.isBetween(initMonth, endMonth, null, '[]')
+          })
+        }
+      })
+      array.push(objDates);
+    });
+    this.infoCalendar=array;
   }
-  nextMonth(){
-    this.objElement={
-      date:moment(this.objElement['formatDate']).add(1,'M').format('MMMM YYYY'),
-      formatDate:moment(this.objElement['formatDate']).add(1,'M')
+  nextMonth() {
+    this.objElement = {
+      date: moment(this.objElement['formatDate']).add(1, 'M').format('MMMM YYYY'),
+      formatDate: moment(this.objElement['formatDate']).add(1, 'M')
     }
     this.initCalendar(this.objElement['formatDate']);
   }
-  previousMonth(){
-    this.objElement={
-      date:moment(this.objElement['formatDate']).subtract(1,'M').format('MMMM YYYY'),
-      formatDate:moment(this.objElement['formatDate']).subtract(1,'M')
+  previousMonth() {
+    this.objElement = {
+      date: moment(this.objElement['formatDate']).subtract(1, 'M').format('MMMM YYYY'),
+      formatDate: moment(this.objElement['formatDate']).subtract(1, 'M')
     }
     this.initCalendar(this.objElement['formatDate']);
   }
